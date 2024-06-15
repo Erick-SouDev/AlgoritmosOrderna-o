fc4f560c30bb4176124aac18afd630b635d6abd4
erickbrunopequenodossantos@outlook.com <erickbrunopequenodossantos@outlook.com>
2024-06-14 23:46:47 -0300

    alterado

--- src/algoritmo/vetor/aula/Vetor.java
+++ src/algoritmo/vetor/aula/Vetor.java
@@ -1,7 +1,5 @@
 package algoritmo.vetor.aula;
 
-import java.util.Arrays;
-
 public class Vetor {
 
 	private String elementos[];
@@ -17,10 +15,10 @@ public class Vetor {
 			this.elementos[tamanho] = elemento;
 			this.tamanho++;
 		} else {
-          throw new Exception("O vetor estar cheior! Error " );
-	 	}
+			throw new Exception("O vetor estar cheior... ");
+		}
 	}
-	
+
 	public int getSizeVector() {
 		return this.tamanho;
 	}
@@ -29,32 +27,54 @@ public class Vetor {
 	public String toString() {
 		StringBuilder builder = new StringBuilder();
 		builder.append("[");
-		for(int index = 0 ; index < this.tamanho-1 ; index++ ) {
+		for (int index = 0; index < this.tamanho - 1; index++) {
 			builder.append(this.elementos[index]);
 			builder.append(",");
 		}
-		if(tamanho >0) {
-			builder.append(elementos[tamanho-1]);
+		if (tamanho > 0) {
+			builder.append(elementos[tamanho - 1]);
 		}
 		builder.append("]");
 
 		return builder.toString();
 	}
-	
+
 	public String getValueByIndexVector(int posicaoBuscada) throws Exception {
-		// buscar elemento no vetor pela posicao didgitada pelo usuario;
-		// retorna valor caso existe e esteja na posicao correta
-		// caso a posicao nao estiver no limite do array gerar um exeption de erro
-		// caso contrario retorna o valor
-		if(posicaoBuscada >= 0 && posicaoBuscada < tamanho  ) {
+		if (posicaoBuscada >= 0 && posicaoBuscada < tamanho) {
 			return elementos[posicaoBuscada];
-		}else {
-			 throw new IllegalArgumentException("Erro! Posição Invalida");
+		} else {
+			throw new IllegalArgumentException("Erro! Posição Invalida");
 		}
 	}
 
-	
-	
-	
-	
+	public int searchPositionElementVector(String value) throws Exception {
+		for (int pos = 0; pos < elementos.length; pos++) {
+			if (elementos[pos] == value) {
+				return pos;
+			}
+		}
+
+		return -1;
+
+	}
+
+	public Boolean checkElementExists(String value) {
+		Boolean existe = false;
+		for (int pos = 0; pos < elementos.length; pos++) {
+			if (elementos[pos] == value) {
+				existe = true;
+				break;
+			} else {
+				existe = false;
+
+			}
+		}
+
+		if (existe) {
+			return true;
+		}
+		return existe;
+
+	}
+
 }
--- src/algoritmo/vetor/teste/BuscarElementoVetorTeste.java
+++ src/algoritmo/vetor/teste/BuscarElementoVetorTeste.java
@@ -11,6 +11,10 @@ public class BuscarElementoVetorTeste {
 		vetor.addVectorElement("valor 3");
 		vetor.addVectorElement("valor 4");
 
+		var existe = vetor.checkElementExists("valor 1");
+		var posicaoVetor = vetor.searchPositionElementVector("valor 6");
+		System.out.println(posicaoVetor);
+
 		
 		System.out.println(vetor.getValueByIndexVector(3));
 	}
